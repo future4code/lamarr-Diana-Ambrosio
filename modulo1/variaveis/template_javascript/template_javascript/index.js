fbd53fa7be646640ac0c9dd7cdf701ab780c9868
diana <dianaapam@gmail.com>
2022-04-13 17:30:23 -0300

    exercicio de variaveis

---
+++ modulo1/variaveis/template_javascript/template_javascript/index.js
@@ -0,0 +1,61 @@
+ //EXERCÍCIO 1 
+let a = 10
+let b = 10
+console.log(b)
+//vai me retornar no console a resposta 10
+//ai defino agora outro valor para a variavel b
+b = 5 //aqui eu altero o valor da variavel
+console.log(a,b) // vai me retornar no console 10 e 5. Já que alterei o valor da variavel b.
+
+
+//EXERCÍCIO 2
+let a = 10
+let b = 20
+c = a //o valor de c é igual ao valor de a,que será 10.
+b = c //o valor de b é igual ao valor de c,que será 10.
+a = b //o valor de a é igual ao valor de b, que será 20.
+console.log(a,b,c) //vai retornar 20, 10 e 10.
+
+
+//EXERCÍCIO 3
+let cargaHorariaDiaria = prompt("quantas horas você trabalha por dia?") 
+let remuneracaoDiaria = prompt("quanto você recebe por dia?")
+alert (`voce recebe ${t/p} por hora`) // vai retornar o cargaHorariaDiaria e remuneracaoDiaria.
+
+//EXERCÍCIO DE ESCRITA DE CÓDIGO 1
+let nome
+let idade
+console.log (typeof nome)
+console.log (typeof idade)  
+
+nome = prompt("Seu Nome?")
+idade = prompt("Sua Idade?") /* impresso na tela (seu nome? eu respondi Diana, 
+ e depois sua idade? eu respondi 25, e abaixo disse indefined)
+*/
+
+console.log ("olá", nome, "voce tem", idade, "anos".) /* vai imprimir, ola Diana
+ voce tem 25 anos. */
+
+
+
+//EXERCÍCIO 2
+const pergunta1 = prompt("Você gosta de pizza? (sim ou nao)") // retorna sim
+const pergunta2 = prompt("Você gosta de ler? (sim ou nao)") // retorna sim
+const pergunta3 = prompt("Você gosta de estudar? (sim ou nao)") // retorna sim
+const resposta = prompt ("Sua Resposta", pergunta1, " ", pergunta2, " ", pergunta3) 
+//retorna sim
+
+//EXERCÍCIO 3
+const a = 10
+const b = 25
+const c = 10
+console.log ("o novo valor de a é" , a=b)
+console.log ("o novo valor de b é", b=a) // nao consegui fazer :(
+
+
+
+
+
+
+
+
